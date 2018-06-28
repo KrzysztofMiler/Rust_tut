9906cba6bc2d7bc2476918b4ee9a2242e57de89b
Krzychu <krzy.miler@gmail.com>
2018-06-28 22:35:43 +0200

    match

--- Zgaduj/src/main.rs
+++ Zgaduj/src/main.rs
@@ -2,13 +2,14 @@ extern crate rand;
 
 use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Zgaduj liczbe!");
 
     let sec = rand::thread_rng().gen_range(1,101);
     println!("sikrit numba {}",sec );
-
+loop{
     println!("Zgaduj");
 
     let mut zgad = String::new();
@@ -16,5 +17,19 @@ fn main() {
     io::stdin().read_line(&mut zgad)
         .expect("nic nie wpisano");
 
+    let zgad:u32=zgad.trim().parse()
+        .expect("liczba pls");
+
     println!("Zgadłeś {}",zgad );
+
+    match zgad.cmp(&sec){
+        Ordering::Less=> println!("za nisko", ),
+        Ordering::Greater=> println!("za wysoko", ),
+        Ordering::Equal=> {
+            println!("gratz", );
+            break;
+        }
+    }
+}
+
 }
