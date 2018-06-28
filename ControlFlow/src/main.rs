3adc2b150c2db4ab7d5bbcc4f6ffe9c015946876
Krzychu <krzy.miler@gmail.com>
2018-06-28 17:18:28 +0200

    +3

--- ControlFlow/src/main.rs
+++ ControlFlow/src/main.rs
@@ -18,4 +18,27 @@ fn main() {
         6
     };
     println!("wychdozi {}", num);
+
+
+    let mut i = 0;
+    loop{
+        i = i+1;        
+        println!("zrobione razy{}",i );
+        if i ==5{
+            break
+        }
+    }
+
+    let mut n =4;
+    while   n !=0{
+        println!("{}!", n);
+        n = n-1;
+    }
+
+    let a = [10,20,30,40,50];
+
+    for ele in a.iter(){
+        println!("val is{}",ele );
+    }
+
 }
