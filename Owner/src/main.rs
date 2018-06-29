bc53989049216f5f2ceda8be2d14333b0ae5affa
Krzychu <krzy.miler@gmail.com>
2018-06-29 13:23:02 +0200

    Move

--- Owner/src/main.rs
+++ Owner/src/main.rs
@@ -4,4 +4,10 @@ let mut s  = String::from("hello");
 s.push_str(", wardo"); //push_str doddaje do stringa dlatego str mut
 print!("{}",s);
 
+let s1 = String::from("hello");
+let s2 = s1;
+//gdy robimy kopie to przekazujemy tylko pointer i unieważniamy
+//nasz pkt który kopiowaliśmy czyli s1
+println!("{}, world!", s2);
+
 }
