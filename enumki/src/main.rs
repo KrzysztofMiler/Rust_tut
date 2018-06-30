d0a71add19fb5c2fa229ef981c617610d79f2c17
Krzychu <krzy.miler@gmail.com>
2018-06-30 18:34:42 +0200

    koniec enumek, obczajic Option<T> bardziej

--- enumki/src/main.rs
+++ enumki/src/main.rs
@@ -8,3 +8,19 @@ fn main() {
     let dom = IpAddrKind::V4(127, 0, 0, 1);
     let loopback = IpAddrKind::V6(String::from("::1"));
 }
+
+
+//enum Message { 
+ //   Quit,
+//    Move { x: i32, y: i32 },
+//    Write(String),
+//    ChangeColor(i32, i32, i32),
+//} //Przykøad æe enumki mogá mie© w sobie wszystko nawet inne enumy
+/*impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
+}
+
+let m = Message::Write(String::from("hello"));
+m.call();*/
