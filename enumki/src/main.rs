f74267bc1aa3a782211b29dedc6dbb60f3a52fdf
Krzychu <krzy.miler@gmail.com>
2018-06-30 18:20:47 +0200

    enumki 3 adv.

--- enumki/src/main.rs
+++ enumki/src/main.rs
@@ -1,10 +1,10 @@
 #![allow(unused_variables)]
 fn main() {
     enum IpAddrKind {
-        V4(String),
+        V4(u8, u8, u8, u8),
         V6(String),
     }
 
-    let dom = IpAddrKind::V4(String::from("127.0.0.1"));
+    let dom = IpAddrKind::V4(127, 0, 0, 1);
     let loopback = IpAddrKind::V6(String::from("::1"));
 }
