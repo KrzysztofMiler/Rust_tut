2d1218e6750840cb52623e1d3d76eabd16990481
Krzychu <krzy.miler@gmail.com>
2018-06-30 18:19:10 +0200

    Enumki 2

--- enumki/src/main.rs
+++ enumki/src/main.rs
@@ -1,22 +1,10 @@
 #![allow(unused_variables)]
 fn main() {
     enum IpAddrKind {
-        V4,
-        V6,
+        V4(String),
+        V6(String),
     }
 
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
-    }
-
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let dom = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
 }
