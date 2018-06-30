00c84ee9c9fbae8e28c12c5e4f0ffca9c5dc84d8
Krzychu <krzy.miler@gmail.com>
2018-06-30 17:40:19 +0200

    Prost 3 Dodano STRUKTURY i ich wyświetlanie

--- prostokont/src/main.rs
+++ prostokont/src/main.rs
@@ -1,13 +1,24 @@
+#[derive(Debug)]//konieczne by można było wyświetlić przez debug
+struct Prost {
+    wys : u32,
+    szer :u32,
+}
+
+
+
 fn main() {
-    let  prost = (50,30);
+    let  prost1 = Prost {szer:30,wys:50};
     
+    println!("prost 1 ma {:?}",prost1 );//:? powoduje że zamiast display 
+    //mamy wyjście na debug, jeśli użyje :#? to dostaje z enterami zformatowane
 
     println!(
         "prostokont ma pole {} pikseli kwadrat",
-        pole(prost)
+        pole(&prost1)
     );
 }
 
-fn pole(wymiar: (u32,u32)) -> u32 {
-    wymiar.0*wymiar.1
+fn pole(prost: &Prost) -> u32 {
+    prost.wys*prost.szer
+    
 }
