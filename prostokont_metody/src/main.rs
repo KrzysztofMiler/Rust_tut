97946ee3008b3cefb44afe30870af326458f6dd3
Krzychu <krzy.miler@gmail.com>
2018-06-30 18:05:18 +0200

    Prost 7 finito

--- prostokont_metody/src/main.rs
+++ prostokont_metody/src/main.rs
@@ -4,12 +4,15 @@ struct Prost {
     szer: u32,
 }
 
-impl Prost {
+impl Prost {//btw. moæna to roazbi© an wiele bloków impl
     fn pole(&self) -> u32 {
         self.wys * self.szer
     }
     fn zmiesci(&self, other: &Prost) -> bool {
         self.szer > other.szer && self.wys > other.wys
+    }
+    fn kwad(size:u32) -> Prost{
+        Prost{wys:size,szer:size}
     }
 }
 
@@ -24,4 +27,7 @@ fn main() {
 
     println!("czy prost 1 zmieści prost 2 ? {}", prost1.zmiesci(&prost2));
     println!("czy prost 1 zmieści prost 3 ? {}", prost1.zmiesci(&prost3));
+
+    let sq = Prost::kwad(3);//associated function ta tworzy nowá instancje stucta
+    //Prost dla kwadratu
 }
