efb25dc4034d6530a4c674ec5e30bfdfa1ac153f
Krzychu <krzy.miler@gmail.com>
2018-06-30 18:00:13 +0200

    Prost 5 metody cd.

--- prostokont_metody/src/main.rs
+++ prostokont_metody/src/main.rs
@@ -8,17 +8,20 @@ impl Prost {
     fn pole(&self) -> u32 {
         self.wys * self.szer
     }
+    fn zmiesci(&self, other: &Prost) -> bool {
+        self.szer > other.szer && self.wys > other.wys
+    }
 }
 
 fn main() {
     let prost1 = Prost { szer: 30, wys: 50 };
     let prost2 = Prost { szer: 20, wys: 40 };
-    let prost1 = Prost { szer: 40, wys: 56 };
+    let prost3 = Prost { szer: 40, wys: 56 };
 
     println!("prost 1 ma {:?}", prost1);
 
     println!("prostokont ma pole {} pikseli kwadrat", prost1.pole()); //tak przekazywać do metod
 
-    //println!("czy prost 1 zmieści prost 2 ? {}", prost1.zmiesci(&prost2));
-    //println!("czy prost 1 zmieści prost 3 ? {}", prost1.zmiesci(&prost3));
+    println!("czy prost 1 zmieści prost 2 ? {}", prost1.zmiesci(&prost2));
+    println!("czy prost 1 zmieści prost 3 ? {}", prost1.zmiesci(&prost3));
 }
