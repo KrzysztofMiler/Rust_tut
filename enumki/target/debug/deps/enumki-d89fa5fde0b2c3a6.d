887952e6453e71f92fa0e6c14b6e5065f3f29e8b
Krzychu <krzy.miler@gmail.com>
2018-06-30 18:15:51 +0200

    Enum 1 czyli po chlopsku robione ze struct

---
+++ enumki/target/debug/deps/enumki-d89fa5fde0b2c3a6.d
@@ -0,0 +1,5 @@
+c:\Users\Nexus\Documents\Rust_tut\Rust_tut\enumki\target\debug\deps\enumki-d89fa5fde0b2c3a6.exe: src\main.rs
+
+c:\Users\Nexus\Documents\Rust_tut\Rust_tut\enumki\target\debug\deps\enumki-d89fa5fde0b2c3a6.d: src\main.rs
+
+src\main.rs:
---
+++ enumki/target/debug/enumki.d
@@ -0,0 +1,1 @@
+c:\Users\Nexus\Documents\Rust_tut\Rust_tut\enumki\target\debug\enumki.pdb: c:\Users\Nexus\Documents\Rust_tut\Rust_tut\enumki\src\main.rs
