4fa91e8a8487551a6b880eed07ee8e20573f7790
Krzychu <krzy.miler@gmail.com>
2018-07-01 20:15:20 +0200

    Wszysko ma swój osobny plik

---
+++ komunikator/target/debug/deps/komunikator-50dd14a8e0d6f55e.d
@@ -0,0 +1,6 @@
+c:\Users\Nexus\Documents\Rust_tut\Rust_tut\komunikator\target\debug\deps\komunikator-50dd14a8e0d6f55e.rmeta: src\lib.rs src\client.rs
+
+c:\Users\Nexus\Documents\Rust_tut\Rust_tut\komunikator\target\debug\deps\komunikator-50dd14a8e0d6f55e.d: src\lib.rs src\client.rs
+
+src\lib.rs:
+src\client.rs:
